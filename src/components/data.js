ebc0278ef1a6dc2823239398e98c1d9300791810
Okpo-Abasi <emmajasperdr@gmail.com>
2023-09-11 00:54:52 +0100

    React quiz project

---
+++ src/components/data.js
@@ -0,0 +1,107 @@
+const data = [
+{
+    id : 1,
+    question : "Rolex is a company that specializes in what type of product?",
+    answer : [
+        {
+            text : "Phone",
+            correct : false
+        },
+
+        {
+            text : "Watches",
+            correct : true
+        },
+
+        {
+            text : "Food",
+            correct : false
+        },
+
+        {
+            text : "Cosmetics",
+            correct : false
+        },
+    ]
+},
+
+{
+    id : 2,
+    question : "When was the website Facebook launched?",
+    answer : [
+        {
+            text : "2004",
+            correct : true
+        },
+
+        {
+            text : "2005",
+            correct : false
+        },
+
+        {
+            text : "2006",
+            correct : false
+        },
+
+        {
+            text : "2007",
+            correct : false
+        },
+    ]
+},
+
+{
+    id : 3,
+    question : "Which is the best software company in Nigeria?",
+    answer : [
+        {
+            text : "Facebook",
+            correct : false
+        },
+
+        {
+            text : "Nigeria Coders Academy",
+            correct : false
+        },
+
+        {
+            text : "Ogtech Networks Ltd",
+            correct : true
+        },
+
+        {
+            text : "Soft Space Ltd",
+            correct : false
+        },
+    ]
+},
+
+{
+    id : 4,
+    question : "Who played the character of Harry Potter in movie?",
+    answer : [
+        {
+            text : "OkpoAbasi Emmanuel",
+            correct : false
+        },
+
+        {
+            text : "Leonardo Di Caprio",
+            correct : false
+        },
+
+        {
+            text : "Denzel Washington",
+            correct : false
+        },
+
+        {
+            text : "Daniel Red-Cliff",
+            correct : true
+        },
+    ]
+},
+]
+
+export default data;
